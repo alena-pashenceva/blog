66d9e699423102e45db3bf1aba2d713f251bcd94
alena-pashenceva <alena.pashenceva@gmail.com>
2014-02-26 20:38:30 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogLost::Application.config.secret_key_base = '37d8cf27c81ff04929942590a1e4353a1d127fcdd8c8c8fea703d1f5ed38c17d80ff087ceb8a606cb1bb61491be63b413c8851fe5cd7033b57dcf56c6dce18d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogLost::Application.config.session_store :cookie_store, key: '_blog_lost_session'
